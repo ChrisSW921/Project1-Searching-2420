6ec9ba5c94e7b2162924dbb2c24350c66b59bca2
Chris <withscottc921@gmail.com>
2020-06-30 10:59:53 -0600

    Wrote the linear_search function

--- search.py
+++ search.py
@@ -1,19 +1,31 @@
 """Search project"""
 
 import recursioncounter
-import test_search
+# import test_search
 
 
-def linear_search():
-    """A function that performs the linear search. """
-    print("Linear search")
+def linear_search(lyst, target):
+    """A function that performs the linear search."""
+    if all(isinstance(x, int) for x in lyst) and type(target) == int:
+        if target in lyst:
+            return True
+        else:
+            return False
+    else:
+        raise ValueError
 
 
-def recursive_binary_search():
+
+def recursive_binary_search(lyst, target):
     """A function that performs the recursive binary search. """
     print("Recursive binary search")
 
 
-def jump_search():
+def jump_search(lyst, target):
     """A function that performs the jump search. """
     print("Jump search")
+
+
+linear_search([1, 2, 3, 4, 5, 6, 7, 9], 5)
+
+

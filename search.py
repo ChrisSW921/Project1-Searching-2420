20534846398160c0240fdc24e6c400ca908d5fda
Chris <withscottc921@gmail.com>
2020-06-30 09:05:10 -0600

    Added pylint to project and began work on functions

--- search.py
+++ search.py
@@ -1,3 +1,19 @@
+"""Search project"""
+
 import recursioncounter
 import test_search
 
+
+def linear_search():
+    """A function that performs the linear search. """
+    print("Linear search")
+
+
+def recursive_binary_search():
+    """A function that performs the recursive binary search. """
+    print("Recursive binary search")
+
+
+def jump_search():
+    """A function that performs the jump search. """
+    print("Jump search")
